f557cbe5268c9e3cb57c2db99eadd49f2034468d
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2022-11-08 10:45:58 -0300

    third commit

--- src/app/components/if-render/if-render.component.ts
+++ src/app/components/if-render/if-render.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IfRenderComponent implements OnInit {
 
-  
+  canShow: boolean = true;
+  name = "Jeniffer"
 
   constructor() { }
 
