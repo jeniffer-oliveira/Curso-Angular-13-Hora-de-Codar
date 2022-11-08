e3219f5607d844a3eb191f43e3fe04c0a9743224
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2022-11-08 14:37:21 -0300

    fourth commit

---
+++ src/app/components/change-number/change-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-number',
+  templateUrl: './change-number.component.html',
+  styleUrls: ['./change-number.component.css']
+})
+export class ChangeNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
