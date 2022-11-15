5a83e2e112d0f1854dabd8a30e32bd294fed36e7
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2022-11-15 19:04:40 -0300

    httpclient

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -12,12 +12,7 @@ import { ListService } from 'src/app/services/list.service';
 })
 export class ListRenderComponent implements OnInit {
 
-  animals: Animal[] = [
-    {name: 'Turca', type: 'Dog', age: 4},
-    {name: 'Tom', type: 'Cat', age: 10},
-    {name: 'Frida', type: 'Dog', age: 5},
-    {name: 'Bob', type: 'Horse', age: 1},
-  ]
+  animals: Animal[] = [];
 
   animal: Animal = {
     name: 'Teste',
@@ -27,7 +22,9 @@ export class ListRenderComponent implements OnInit {
 
   animalDetails = '';
 
-  constructor(private listService: ListService) { }
+  constructor(private listService: ListService) {
+    this.getAnimals()
+   }
 
   ngOnInit(): void { }
 
@@ -38,6 +35,10 @@ export class ListRenderComponent implements OnInit {
   removeAnimal(animal: Animal){
     console.log('Removendo animal...');
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void{
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 }
 
